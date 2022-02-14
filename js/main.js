46ebca207b0118e9b6c07824a59070c125d897e1
sergii1978 <sens78@gmail.com>
2022-02-14 02:05:50 +0200

    correct slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(function(){
-
+  $('.header-slider').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/svg/prev.svg" alt="previus"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/svg/next.svg" alt="next"></button>',
+    vertical: true
+  });
 });
