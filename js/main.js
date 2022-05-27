4a3c44645a1535cf6fc32f1fc211bc9aa23b2270
Sergii Storcheus <sens78@gmail.com>
2022-05-28 00:57:17 +0300

    add section benefits
    add section benefits

--- js/main.js
+++ js/main.js
@@ -5,4 +5,23 @@ $(function(){
     nextArrow: '<button type="button" class="slick-next"><img src="img/svg/next.svg" alt="next"></button>',
     vertical: true
   });
+
+  
+  $('.product-list').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    asNavFor: '.content-slider_wrapper',
+    vertical: true,
+    focusOnSelect: true,
+    prevArrow: '<button type="button" class="product-prev"><img src="img/svg/arrow_up.svg" alt="previus"></button>',
+    nextArrow: '<button type="button" class="product-next"><img src="img/svg/arrow_down.svg" alt="next"></button>',
+  });
+
+  $('.content-slider_wrapper').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.product-list',
+    arrows: false,
+    fade: true
+  });
 });
