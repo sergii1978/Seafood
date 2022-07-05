62b36d16ef15f239082614abb1645220f13fd11f
sergii1978 <sens78@gmail.com>
2022-07-05 23:45:28 +0300

    correct header menu

--- js/main.js
+++ js/main.js
@@ -24,4 +24,54 @@ $(function(){
     arrows: false,
     fade: true
   });
+  
+  /* Меню */
+  $('ul.header-menu a[href^="#"').click(function () {
+    var target = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 800);
+    $('ul.menu a[href^="#"').css({
+        'color': '#212121'
+    });
+    $(this).css({
+        'color': '#004bee'
+    });
+    return false;
+});
+
+/* Выпадающее меню */
+$('.menu-icon').click(function () {
+    $('.nav-hide').slideToggle(500);
+    $('.nav-hide').css({
+        'display': 'flex',
+        'justify-content': 'center'
+    });
+    $('ul.nav-list').css({
+        'display': 'flex',
+        'flex-direction': 'column',
+        'justify-content': 'center',
+        'background': 'transparent'
+    });
+    $('.nav-list li').css({
+      'margin-left': '0',
+      'margin-top': '5px',
+      'margin-bottom': '5px'
+    });
+    $('.header_slideshow').css({
+        'top': '250px',
+    });
+
+    $('.header_slideshow').css({
+        'top': '250px',
+    });
+    if ($('.menu-icon').html() == '<img src="img/svg/bars.svg">') {
+      $(this).html('<img src="img/svg/xmark.svg">');
+    } else {
+      $(this).html('<img src="img/svg/bars.svg">');
+      $('.header_slideshow').css({
+        'top': '0px',
+    });
+    }
+});
 });
